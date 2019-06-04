6bbfc1afe918f5cb988fa411f6e899c88cf361b3
MehranDahsti <mehran19.md@gmail.com>
2019-06-05 00:25:34 +0430

    In SecurityController new section has been added for signUp user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -20,5 +27,43 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * This action has been used for signUp User
+     *
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return Response
+     * @Route("/sign-up", name="sign_up")
+     * @author Mehran
+     */
+    public function signUp(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user = new User();
+        $entityManager = $this->getDoctrine()->getManager();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $user->getPassword()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+//                $entityManager->getConnection()->commit();
+
+            $this->addFlash("success", "New User has been created successfully :);)");
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/SignUp.html.twig', [
+            'form' => $form->createView(),
+            'errors' => $form->getErrors()
+        ]);
     }
 }
