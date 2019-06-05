2dd540b0f293aa1797364a1bf0bb9fd9a653165d
MehranDahsti <mehran19.md@gmail.com>
2019-06-05 13:38:54 +0430

    Two new action has been added to Productcontroller for show list of product and Add New Product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,10 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductType;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
+ */
 class ProductController extends AbstractController
 {
     /**
@@ -21,6 +28,46 @@ class ProductController extends AbstractController
 
         return $this->render('product/list.html.twig', [
             'products' => $products,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/product/add", name="add_product")
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
+     */
+    public function add(Request $request)
+    {
+        $product = new Product();
+        $entityManager = $this->getDoctrine()->getManager();
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->getConnection()->beginTransaction();
+            try {
+//                $product->setCreatedBy($this->getUser());
+//                $product->setUpdatedBy($this->getUser());
+                $entityManager->persist($product);
+                $entityManager->flush();
+                $entityManager->getConnection()->commit();
+
+                $this->addFlash("success", "New Product has been created successfully :);)");
+                return $this->redirectToRoute('product_list');
+            } catch (Exception $e) {
+                $entityManager->getConnection()->rollBack();
+                echo "<pre>";
+                print_r($e->getMessage());
+                die();
+                $this->addFlash("error", "There is some problem you can not create Product :(:(");
+                return $this->redirectToRoute('add_product');
+            }
+        }
+
+        return $this->render('product/add.html.twig', [
+            'form' => $form->createView(),
+            'errors' => $form->getErrors()
         ]);
     }
 }
