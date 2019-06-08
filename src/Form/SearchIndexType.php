01abe013c7ea79b7bfd1a9fd4fe631f346507039
MehranDahsti <mehran19.md@gmail.com>
2019-06-08 20:20:03 +0430

    Two new Type has been added to project in named SearchIndexType and  SearchVariantType types for handle search form

---
+++ src/Form/SearchIndexType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchIndexType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, ['required' => true])
+            ->add('description', TextType::class, ['required' => true])
+            ->add('variants', CollectionType::class, [
+                'entry_type' => SearchVariantType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'label' => false
+            ])
+            ->add('search', SubmitType::class, ['label' => 'Search']);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
---
+++ src/Form/SearchVariantType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Variant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchVariantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('color', TextType::class, ['required' => true])
+            ->add('price', TextType::class, ['required' => true]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Variant::class,
+        ]);
+    }
+}
