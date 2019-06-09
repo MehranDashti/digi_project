1235fbc4e9041ea6b7586f4c332709627cd6500b
MehranDahsti <mehran19.md@gmail.com>
2019-06-09 23:03:49 +0430

    The Product Controller has been refactor to Php7 and apply new rules in it

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -4,8 +4,11 @@ namespace App\Repository;
 
 use App\Entity\Product;
 use App\Wrapper\ElasticWrapper;
+use App\Wrapper\RedisWrapper;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
 
 /**
  * @method Product|null find($id, $lockMode = null, $lockVersion = null)
@@ -25,11 +28,11 @@ class ProductRepository extends ServiceEntityRepository
      *  One : When user load first page and see all product
      *  Second : When user search particular product according to title, description, color and price
      *
-     * @param null $data
+     * @param Product $data
      * @return array
      * @author Mehran
      */
-    public function fetchProduct($data = null): array
+    public function fetchProduct(Product $data = null): array
     {
         $result = [];
         if (is_null($data)) {
@@ -39,12 +42,50 @@ class ProductRepository extends ServiceEntityRepository
                 $result[$key]['title'] = $product->getTitle();
                 $result[$key]['description'] = $product->getDescription();
             }
-
             return $result;
         }
 
-        return ElasticWrapper::search()->searchIndex($data);
+        return $this->searchElasticIndex($data);
+    }
+
+    /**
+     * @param Product $data
+     * @return array
+     * @author Mehran
+     */
+    public function searchElasticIndex(Product $data):array
+    {
+        $response = ElasticWrapper::search()->searchIndex($data);
+        if (!empty($response)) {
+            foreach ($response as $key => $hit) {
+                $cacheProduct = RedisWrapper::action()->fetchCacheData('product_' . $hit['_id']);
+                if (empty($cacheProduct)) {
+                    $cacheProduct = $this->setProductCache($hit['__id']);
+                } else {
+                    $cacheProduct = json_decode($cacheProduct, true);
+                }
+                $result[$key] = array_merge($cacheProduct, ['id' => $hit['_id']]);
+            }
+            return $result;
+        }
+        return [];
     }
 
 
+    /**
+     * @param integer $product_id
+     * @return array
+     * @author Mehran
+     */
+    protected function setProductCache(Integer $product_id): array
+    {
+        $product = $this->findOneBy(['id' => $product_id]);
+        $productData = [
+            'title' => $product->getTitle(),
+            'description' => $product->getDescription()
+        ];
+        RedisWrapper::action()->initializeCache('product_' . $product->getId(), $productData);
+
+        return $productData;
+    }
 }
